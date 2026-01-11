e691baef737b3554177bacb31010ea1dcc78641a
Zijin Zhang <58776897+Beforerr@users.noreply.github.com>
2026-01-10 23:50:41 -0800

    docs: fix (#3)

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,3 +7,12 @@ using Aqua
 end
 
 include("fido.jl")
+
+@testset "Maps" begin
+    using PythonCall
+    pyimport("sunpy.data.sample")
+    m = SunPy.Map(sunpy.data.sample.AIA_171_IMAGE)
+    @test m isa AbstractArray
+    @test size(m) == (1024, 1024)
+    @test m[1] == -95.92475f0
+end
